8099731041503ce61a8f1e85f62504fa1d5f9de8
ekkolesnik <grid.box@gmail.com>
2020-02-14 00:00:30 +0300

    Добавил тень к аватаркам на контроллере с друзьями

--- Lesson_1/Cell/FriensCell.swift
+++ Lesson_1/Cell/FriensCell.swift
@@ -14,4 +14,22 @@ class FriensCell: UITableViewCell {
    
     @IBOutlet weak var ImagePic: UIImageView!
     
+    @IBOutlet weak var ViewImage: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        ViewImage.layer.cornerRadius = ViewImage.frame.height / 2
+        
+        ViewImage.layer.shadowColor = UIColor.black.cgColor
+        ViewImage.layer.shadowOpacity = 0.7
+        ViewImage.layer.shadowRadius = 6
+        ViewImage.layer.shadowOffset = .zero
+        
+        ImagePic.layer.cornerRadius = ImagePic.frame.height / 2
+        
+    }
+    
+    
+    
 }
